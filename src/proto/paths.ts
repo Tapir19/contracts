ceabdfd312fbe1c4ce519fb88f233a2a98f1a38e
Aleksey Astashonok <lextapir191919@gmail.com>
2026-02-14 15:30:13 +0300

    feat: add account service proto

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -2,4 +2,5 @@ import { join } from "node:path";
 
 export const PROTO_PATHS = {
   AUTH: join(__dirname, "../../proto/auth.proto"),
+  ACCOUNT: join(__dirname, "../../proto/account.proto"),
 } as const;
