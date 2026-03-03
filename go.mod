35152442de4f694588e2f0bf5f3d491f4032060c
Aleksey Astashonok <lextapir191919@gmail.com>
2026-03-03 13:41:24 +0300

    feat: add media proto

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tapir19/contracts
+
+go 1.25.0
---
+++ scripts/generate-go.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+
+set -e
+
+echo "Generating Go protobuf (media)..."
+
+protoc -I ./proto \
+    --go_out=./gen/go \
+    --go-grpc_out=./gen/go \
+    --go-opt=module=github.com/tapir19/contracts \
+    --go-grpc_opt=module=github.com/tapir19/contracts \
+    ./proto/media.proto
+
+echo "Go generation complete."
