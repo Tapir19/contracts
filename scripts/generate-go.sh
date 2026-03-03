0669568d4c4e995513c69d6d226914f3668163b0
Aleksey Astashonok <lextapir191919@gmail.com>
2026-03-03 14:24:18 +0300

    fix: align go_package and generation paths

--- scripts/generate-go.sh
+++ scripts/generate-go.sh
@@ -1,12 +1,12 @@
-# !/bin/bash
+#!/bin/bash
 
 set -e
 
 echo "Generating Go protobuf (media)..."
 
 protoc -I ./proto \
-    --go_out=./gen/go \
-    --go-grpc_out=./gen/go \
+    --go_out=. \
+    --go-grpc_out=. \
     --go_opt=module=github.com/tapir19/contracts \
     --go-grpc_opt=module=github.com/tapir19/contracts \
     ./proto/media.proto
